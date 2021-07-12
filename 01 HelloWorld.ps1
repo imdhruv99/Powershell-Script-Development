3e889b8d05c3117a8f34c0b25a5786b6832e2edb
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-07-12 09:30:47 +0530

    [ADDED] Hello World for PS

---
+++ 01 HelloWorld.ps1
@@ -0,0 +1,11 @@
+﻿$firstName = $args[0]
+$lastName = $args[1]
+
+Write-Host "Hello $firstName $lastName"
+
+Write-Host "You are login from $env:COMPUTERNAME"
+
+Write-Host "There is not time to learn about time in time"
+
+# Run the script pass below command in powershell terminal
+# .\01 HelloWorld.ps1 "firsname" "lastname"
---
+++ 02 Param.ps1
@@ -0,0 +1,10 @@
+﻿param($firstName, $lastName)
+
+Write-Host "Hello $firstName $lastName"
+
+Write-Host "You are login from $env:COMPUTERNAME"
+
+Write-Host "There is not time to learn about time in time"
+
+# Run the script pass below command in powershell terminal
+# '.\02 Param.ps1' -firstName <FN> -lastName <LN>
