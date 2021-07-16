eabec450e23c5cc6a648ebfee5b4a21ea1ec9f5c
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-07-16 07:22:36 +0530

    [COMPLETED] Funtions and Exception Handling

---
+++ 27 Function.ps1
@@ -0,0 +1,16 @@
+﻿function calculator
+{
+    $a = [Int](Read-Host "Enter Number")
+    $b = [Int](Read-Host "Enter Number")
+
+    Write-Host "`n"
+    $a + $b
+    $a - $b
+    $a * $b
+    $a / $b
+    $a % $b
+
+    return Get-Date
+}
+
+calculator
---
+++ 28 Try_Catch_Finally.ps1
@@ -0,0 +1,21 @@
+﻿function Exception-Handling {
+
+    try
+    {
+        $items = Get-Item -Path C:\Does\Not\Exist, C:\Windows, $env:APPDATA -ErrorAction Stop
+    }
+    catch [System.Management.Automation.ItemNotFoundException]
+    {
+        # Specific catch block for the exception type
+        # PSItem contains the error record, and TargetObject may contain the actual object raising the error
+        Write-Host ('Could not find folder {0}' -f $PSItem.TargetObject)
+    }
+    finally
+    {
+        # Regardless of whether an error occurred or not, the optional
+        # finally block is always executed.
+        Write-Host 'Always executed'
+    }
+}
+
+Exception-Handling
